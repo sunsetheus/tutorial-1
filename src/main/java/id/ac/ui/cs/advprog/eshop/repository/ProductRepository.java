e3e806f017f2ba2b4244c70cc102e51297f0ded2
sunsetheus <williamsalimrjs@gmail.com>
2024-02-06 15:15:31 +0700

    refactor: remove unnecessary validation and debug prints

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -13,12 +13,6 @@ public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
-        if (product.getProductQuantity() < 1) {
-            System.out.println("Product quantity dibak: " + product.getProductQuantity());
-            throw new IllegalArgumentException("Product quantity must be positive integer");
-        }
-
-        System.out.println("lolos dibak: ");
         product.setProductId(UUID.randomUUID().toString());
         productData.add(product);
         return product;
