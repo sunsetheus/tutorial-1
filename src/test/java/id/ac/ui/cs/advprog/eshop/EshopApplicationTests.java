817bb83ecf1762da9949768ed64cb58e5cc3e079
sunsetheus <williamsalimrjs@gmail.com>
2024-02-14 18:42:55 +0700

    refactor: remove field injection and use constructor injection instead

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import id.ac.ui.cs.advprog.eshop.controller.ProductController;
@@ -8,10 +9,13 @@ import id.ac.ui.cs.advprog.eshop.controller.ProductController;
 @SpringBootTest
 class EshopApplicationTests {
 
+	@Autowired
+	private ProductController productController;
+
 	@Test
 	void contextLoads() {
 		EshopApplication.main(new String[]{});
-		assertThat(new ProductController()).isNotNull();
+		assertThat(productController).isNotNull();
 	}
 
 }
