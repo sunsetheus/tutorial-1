daa3a838ca3fa0a891eefe1a47a02e9480c9452e
sunsetheus <williamsalimrjs@gmail.com>
2024-02-14 14:39:27 +0700

    feat: add sonar plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,15 @@ plugins {
 	jacoco
 	id("org.springframework.boot") version "3.2.2"
 	id("io.spring.dependency-management") version "1.1.4"
+	id("org.sonarqube") version "4.4.1.3373"
+}
+
+sonar {
+	properties {
+		property("sonar.projectKey", "sunsetheus-adpro")
+		property("sonar.organization", "sunsetheu5")
+		property("sonar.host.url", "https://sonarcloud.io")
+	}
 }
 
 val seleniumJavaVersion = "4.14.1"
