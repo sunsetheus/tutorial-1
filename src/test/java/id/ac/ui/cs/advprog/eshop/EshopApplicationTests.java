65e88c3d9169b071c3deb0107eaa90c4c33fcaf2
sunsetheus <williamsalimrjs@gmail.com>
2024-02-14 18:33:10 +0700

    feat: add assertions for test

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -2,6 +2,8 @@ package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import id.ac.ui.cs.advprog.eshop.controller.ProductController;
 
 @SpringBootTest
 class EshopApplicationTests {
@@ -9,6 +11,7 @@ class EshopApplicationTests {
 	@Test
 	void contextLoads() {
 		EshopApplication.main(new String[]{});
+		assertThat(new ProductController()).isNotNull();
 	}
 
 }
