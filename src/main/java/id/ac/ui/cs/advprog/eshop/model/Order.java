3abf6e965b5d0fae07a450049fc5631423be4df1
sunsetheus <williamsalimrjs@gmail.com>
2024-03-05 08:39:28 +0700

    [REFACTOR] Apply OrderStatus enum check to Order model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,50 +1,44 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
-import lombok.Builder;
-import lombok.Getter;
-
 import java.util.Arrays;
 import java.util.List;
+
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import lombok.Builder;
+import lombok.Getter;
 
 @Builder
 @Getter
 public class Order {
     String id;
-    List <Product> products;
+    List<Product> products;
     Long orderTime;
     String author;
     String status;
 
-    public Order (String id, List <Product> products, Long orderTime, String author) {
+    public Order(String id, List<Product> products, Long orderTime, String author) {
         this.id = id;
         this.orderTime = orderTime;
         this.author = author;
-        this.status = "WAITING_PAYMENT";
+        this.status = OrderStatus.WAITING_PAYMENT.getValue();
 
-        if (products.isEmpty()) {
-            throw new IllegalArgumentException();
+        if (products == null || products.isEmpty()) {
+            throw new IllegalArgumentException("Order must have at least one product");
         } else {
             this.products = products;
         }
     }
 
-    public Order (String id, List <Product> products, Long orderTime, String author, String status) {
+    public Order(String id, List<Product> products, Long orderTime, String author, String status) {
         this(id, products, orderTime, author);
-
-        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
-        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
-            throw new IllegalArgumentException();
-        } else {
-            this.status = status;
-        }
+        this.setStatus(status);
     }
 
     public void setStatus(String status) {
-        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
-        if (Arrays.stream(statusList).noneMatch(item -> (item.equals(status)))) {
-            throw new IllegalArgumentException();
-        } else {
-            this.status = status;
+        if (!OrderStatus.contains(status)) {
+            throw new IllegalArgumentException("Invalid order status");
         }
+
+        this.status = status;
     }
 }
